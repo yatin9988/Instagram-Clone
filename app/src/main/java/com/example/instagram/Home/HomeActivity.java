3038e936abe777e1c603df2ed2fe26628702c984
YATIN <yatin88617@gmail.com>
2020-06-26 10:38:56 +0530

    attached viewpager with the home activity's fragments

--- app/src/main/java/com/example/instagram/Home/HomeActivity.java
+++ app/src/main/java/com/example/instagram/Home/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.instagram.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.instagram.R;
 import com.example.instagram.Utils.BottomNavigationHelper;
+import com.example.instagram.adapter.SectionsPagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -25,5 +33,24 @@ public class HomeActivity extends AppCompatActivity {
         item.setChecked(true);
 
         BottomNavigationHelper.enableNavigation(this,bottomNavigationView);
+        setViewPager();
     }
+
+    private void setViewPager(){
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager viewPager = findViewById(R.id.container);
+        List<Fragment>fragments = new ArrayList<>();
+        fragments.add(new CameraFragment());
+        fragments.add(new HomeFragment());
+        fragments.add(new MessagesFragment());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),fragments);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instagram_black);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
 }
---
+++ app/src/main/java/com/example/instagram/adapter/SectionsPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.instagram.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment>mFragments;
+
+    public SectionsPagerAdapter(FragmentManager fragmentManager,List<Fragment>fragments){
+        super(fragmentManager);
+        this.mFragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
